9270c8b2cf30e839b8c35c089d62e992b4f502f6
Perry Theobald <perrytjr@gmail.com>
2020-10-22 17:11:19 -0700

    apiroutes starting to set up file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,54 @@
+
 var path = require("path");
 
+const Workout = require("../models/workout.js");
 
+module.exports = function(app) {
+
+app.get('/path', function (request, response) {
+    // get all body stuff from request.body object
+    // get all request parameter stuff from request.params object
+    
+    // respond with: response.send()
+
+});
+
+    
+
+app.get('/path', function (request, response) {
+    // get all body stuff from request.body object
+    // get all request parameter stuff from request.params object
+    
+    // respond with: response.send()
+
+});
+
+app.get('/path', function (request, response) {
+    // get all body stuff from request.body object
+    // get all request parameter stuff from request.params object
+    
+    // respond with: response.send()
+
+});
+
+app.get('/path', function (request, response) {
+    // get all body stuff from request.body object
+    // get all request parameter stuff from request.params object
+    
+    // respond with: response.send()
+
+});
+
+
+
+app.get('/path', function (request, response) {
+    // get all body stuff from request.body object
+    // get all request parameter stuff from request.params object
+    
+    // respond with: response.send()
+
+});
+
+
+
+};
