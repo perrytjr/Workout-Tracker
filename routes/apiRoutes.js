2add9829ea30d5367dd7a0aae51c02cb92465753
Perry Theobald <perrytjr@gmail.com>
2020-10-23 15:42:51 -0700

    first two routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,40 +1,51 @@
 
 var path = require("path");
 
-const Workout = require("../models/workout.js");
+const db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
+
+    app.get('/api/workouts', function (req, res) {
+
+        db.Workout.find({})
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+
+    });
+
+    app.put('/api/workouts/: id', ({ body }, res) => {
+        
+        db.Workout.findOneAndUpdate(
+            params.id,
+            { $push: { exercises: body } },
+            { new: true }
+        )
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+
+
 
 app.get('/path', function (request, response) {
     // get all body stuff from request.body object
     // get all request parameter stuff from request.params object
-    
+
     // respond with: response.send()
 
 });
 
-    
-
 app.get('/path', function (request, response) {
     // get all body stuff from request.body object
     // get all request parameter stuff from request.params object
-    
-    // respond with: response.send()
 
-});
-
-app.get('/path', function (request, response) {
-    // get all body stuff from request.body object
-    // get all request parameter stuff from request.params object
-    
-    // respond with: response.send()
-
-});
-
-app.get('/path', function (request, response) {
-    // get all body stuff from request.body object
-    // get all request parameter stuff from request.params object
-    
     // respond with: response.send()
 
 });
@@ -44,7 +55,7 @@ app.get('/path', function (request, response) {
 app.get('/path', function (request, response) {
     // get all body stuff from request.body object
     // get all request parameter stuff from request.params object
-    
+
     // respond with: response.send()
 
 });
--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const morgan = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000
-
+const db = require()
+const db = require('./models');
 const app = express();
 app.use(morgan("dev"));
 
