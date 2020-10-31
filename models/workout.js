96dad99d2af4ddbc1b700fe83f867166640c7abd
Perry Theobald <perrytjr@gmail.com>
2020-10-31 14:45:29 -0700

    duration is now working

--- models/workout.js
+++ models/workout.js
@@ -42,15 +42,21 @@ const workoutSchema = new Schema(
          
         }
       ]
+    },
+      {
+        toJSON: {
+          // include any virtual properties when data is requested
+          virtuals: true
+        }
+      }
+    );
+
+    workoutSchema.virtual("totalDuration").get(function() {
+      // "reduce" array of exercises down to just the sum of their durations
+      return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+      }, 0);
     });
-
-
-    workoutSchema.methods.setDuration = function() {
-      this.update = `${this.duration}`;
-    
-      return this.update;
-    
-    };
   
   const Workout = mongoose.model("Workout", workoutSchema);
 
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -31,18 +31,9 @@ module.exports = function (app) {
       });
   });
 
-  // app.put("/api/workouts/:id", function ({body}, res) {
-  //   const update = new Workout(body);
-  //   update.setDuration();
-  //   Workout.Update(update)
-  //     .then(data =>
 
-  //       res.json(data))
-  //     .catch(err => {
-  //       res.json(err);
-  //     })
-  // });
   app.put("/api/workouts/:id ", ({ body }, res) => {
+    
     Workout.update(body)
       .then(({ _id }) => Workout.findOneAndUpdate({}, { $push: { exercises: _id } }, { new: true }))
       .then(data =>
